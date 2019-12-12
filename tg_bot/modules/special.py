a7b19a9b76cda0255c6694154057b0b5daf01ddc
Amazers03 <56584564+Amazers03@users.noreply.github.com>
2019-12-13 00:12:02 +0530

    Add files via upload

--- tg_bot/modules/special.py
+++ tg_bot/modules/special.py
@@ -2,18 +2,30 @@ from io import BytesIO
 from time import sleep
 from typing import Optional, List
 from telegram import TelegramError, Chat, Message
-from telegram import Update, Bot
+from telegram import Update, Bot, User
+from telegram import ParseMode
 from telegram.error import BadRequest
 from telegram.ext import MessageHandler, Filters, CommandHandler
 from telegram.ext.dispatcher import run_async
-from tg_bot.modules.helper_funcs.chat_status import is_user_ban_protected, bot_admin
+from telegram.utils.helpers import escape_markdown
+from tg_bot.modules.helper_funcs.chat_status import is_user_ban_protected, user_admin, bot_admin
 
+import random
+import telegram
 import tg_bot.modules.sql.users_sql as sql
-from tg_bot import dispatcher, OWNER_ID, LOGGER
+from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, LOGGER
 from tg_bot.modules.helper_funcs.filters import CustomFilters
-
+from tg_bot.modules.disable import DisableAbleCommandHandler
 USERS_GROUP = 4
 
+MESSAGES = (
+    "Happy birthday ",
+    "Heppi burfdey ",
+    "Hep burf ",
+    "Happy day of birthing ",
+    "Sadn't deathn't-day ",
+    "Oof, you were born today ",
+)
 
 @run_async
 def quickscope(bot: Bot, update: Update, args: List[int]):
@@ -78,21 +90,64 @@ def snipe(bot: Bot, update: Update, args: List[str]):
 
 
 @run_async
-@bot_admin
 def getlink(bot: Bot, update: Update, args: List[int]):
     if args:
         chat_id = int(args[0])
     else:
         update.effective_message.reply_text("You don't seem to be referring to a chat")
-    chat = bot.getChat(chat_id)
-    bot_member = chat.get_member(bot.id)
-    if bot_member.can_invite_users:
-        invitelink = bot.get_chat(chat_id).invite_link
-        update.effective_message.reply_text(invitelink)
-    else:
-        update.effective_message.reply_text("I don't have access to the invite link!")
+    for chat_id in args:
+        try:
+            chat = bot.getChat(chat_id)
+            bot_member = chat.get_member(bot.id)
+            if bot_member.can_invite_users:
+                invitelink = bot.exportChatInviteLink(chat_id)
+                update.effective_message.reply_text("Invite link for: " + chat_id + "\n" + invitelink)
+            else:
+                update.effective_message.reply_text("I don't have access to the invite link.")
+        except BadRequest as excp:
+                update.effective_message.reply_text(excp.message + " " + str(chat_id))
+        except TelegramError as excp:
+                update.effective_message.reply_text(excp.message + " " + str(chat_id))
+
+@run_async
+def slist(bot: Bot, update: Update):
+    message = update.effective_message
+    text1 = "My sudo users are:"
+    text2 = "My support users are:"
+    for user_id in SUDO_USERS:
+        try:
+            user = bot.get_chat(user_id)
+            name = "[{}](tg://user?id={})".format(user.first_name + (user.last_name or ""), user.id)
+            if user.username:
+                name = escape_markdown("@" + user.username)
+            text1 += "\n - {}".format(name)
+        except BadRequest as excp:
+            if excp.message == 'Chat not found':
+                text1 += "\n - ({}) - not found".format(user_id)
+    for user_id in SUPPORT_USERS:
+        try:
+            user = bot.get_chat(user_id)
+            name = "[{}](tg://user?id={})".format(user.first_name + (user.last_name or ""), user.id)
+            if user.username:
+                name = escape_markdown("@" + user.username)
+            text2 += "\n - {}".format(name)
+        except BadRequest as excp:
+            if excp.message == 'Chat not found':
+                text2 += "\n - ({}) - not found".format(user_id)
+    message.reply_text(text1 + "\n", parse_mode=ParseMode.MARKDOWN)
+    message.reply_text(text2 + "\n", parse_mode=ParseMode.MARKDOWN)
 
 
+@run_async
+@user_admin
+def birthday(bot: Bot, update: Update, args: List[str]):
+    if args:
+        username = str(",".join(args))
+    bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
+    for i in range(5):
+        bdaymessage = random.choice(MESSAGES)
+        update.effective_message.reply_text(bdaymessage + username)
+   
 @bot_admin
 def leavechat(bot: Bot, update: Update, args: List[int]):
     if args:
@@ -100,42 +155,43 @@ def leavechat(bot: Bot, update: Update, args: List[int]):
         bot.leaveChat(chat_id)
     else:
         update.effective_message.reply_text("You don't seem to be referring to a chat")
-
+        
+        
 __help__ = """
-**Owner only:**
-- /getlink **chatid**: Get the invite link for a specific chat.
+*Owner only:*
+- /getlink *chatid*: Get the invite link for a specific chat.
 - /banall: Ban all members from a chat
-- /leavechat **chatid** : leave a chat
-**Sudo/owner only:**
-- /quickscope **userid** **chatid**: Ban user from chat.
-- /quickunban **userid** **chatid**: Unban user from chat.
-- /snipe **chatid** **string**: Make me send a message to a specific chat.
-- /rban **userid** **chatid** remotely ban a user from a chat
-- /runban **userid** **chatid** remotely unban a user from a chat
-- /Stats: check bot's stats
-- /chatlist: get chatlist
+- /leavechat *chatid* : leave a chat
+
+*Sudo only:*
+- /quickscope *chatid* *userid*: Ban user from chat.
+- /quickunban *chatid* *userid*: Unban user from chat.
+- /snipe *chatid* *string*: Make me send a message to a specific chat.
+- /slist: Gives a list of support and sudo users
+- /rban *chatid* *userid*: remotely ban a user from a chat
+- /runban *chatid* *userid*: remotely unban a user from a chat
+- /stats: check bot's stats
+- /chatlist: get chatlist 
 - /gbanlist: get gbanned users list
 - /gmutelist: get gmuted users list
-- Chat bans via /restrict chat_id and /unrestrict chat_id commands
-**Support user:**
-- /Gban : Global ban a user
-- /Ungban : Ungban a user
-- /Gmute : Gmute a user
-- /Ungmute : Ungmute a user
-Sudo/owner can use these commands too.
-**Users:**
-- /listsudo Gives a list of sudo users
-- /listsupport gives a list of support users
+
+*Admin only:*
+- /birthday *@username*: Spam user with birthday wishes.
 """
+
 __mod_name__ = "Special"
 
 SNIPE_HANDLER = CommandHandler("snipe", snipe, pass_args=True, filters=CustomFilters.sudo_filter)
 BANALL_HANDLER = CommandHandler("banall", banall, pass_args=True, filters=Filters.user(OWNER_ID))
+GETLINK_HANDLER = CommandHandler("getlink", getlink, pass_args=True, filters=Filters.user(OWNER_ID))
+SLIST_HANDLER = CommandHandler("slist", slist, filters=CustomFilters.sudo_filter)
+BIRTHDAY_HANDLER = DisableAbleCommandHandler("birthday", birthday, pass_args=True, filters=Filters.group)
+LEAVECHAT_HANDLER = CommandHandler("leavechat", leavechat, pass_args=True, filters=Filters.user(OWNER_ID))
 QUICKSCOPE_HANDLER = CommandHandler("quickscope", quickscope, pass_args=True, filters=CustomFilters.sudo_filter)
 QUICKUNBAN_HANDLER = CommandHandler("quickunban", quickunban, pass_args=True, filters=CustomFilters.sudo_filter)
-GETLINK_HANDLER = CommandHandler("getlink", getlink, pass_args=True, filters=Filters.user(OWNER_ID))
-LEAVECHAT_HANDLER = CommandHandler("leavechat", leavechat, pass_args=True, filters=Filters.user(OWNER_ID))
 
+dispatcher.add_handler(SLIST_HANDLER)
+dispatcher.add_handler(BIRTHDAY_HANDLER)
 dispatcher.add_handler(SNIPE_HANDLER)
 dispatcher.add_handler(BANALL_HANDLER)
 dispatcher.add_handler(QUICKSCOPE_HANDLER)
